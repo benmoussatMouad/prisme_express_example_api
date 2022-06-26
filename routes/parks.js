a14740e0b2d83444544e1d448f2b92eac56e964e
MounirCHERIF <mounircherif2001@gmail.com>
2022-06-26 03:20:05 +0100

    tables modification

--- routes/parks.js
+++ routes/parks.js
@@ -16,6 +16,8 @@ router.get('/', function(req, res, next) {
                     orderBy: {
                         idHistoriqueParking: 'desc',
                       }
+                },
+                Horaire:{
                 }
             }
         }).then(users => {
